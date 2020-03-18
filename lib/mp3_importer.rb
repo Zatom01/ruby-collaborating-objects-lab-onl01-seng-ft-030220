bd5798e54e718ced84b8f57b56cf1aa2706dcf46
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-18 20:41:36 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,31 @@
+class MP3Importer 
+  
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path=path
+  end 
+  
+  def files 
+    files=[]
+    Dir.new(self.path).each do |file|
+      if file.length >4
+        files << file 
+      end 
+      
+      files
+    end 
+  end 
+  
+  def import 
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end 
+  end 
+  
+  
+  
+  
+  
+  
+end 
