6a76bd4c4aa25c86c91cdeb240403a5a1953178a
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-18 22:06:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   
   def self.new_by_filename(filename)
     song_name=filename.split(" - ")[1]
-    artist_name=filename.split(" - ")[0]
+    artist=filename.split(" - ")[0]
     song=self.new(song_name)
     song.artist_name=artist
     song 
