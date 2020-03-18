f166b3045c50abcec004f431bf02c68a9a7c162e
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-18 21:22:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,22 @@
-class Song 
-  
-  attr_accessor :name, :artist 
-  
-  @@all=[]
-  
+class Song
+
+  attr_accessor :name, :artist
+
   def initialize(name)
-    @name=name 
-    @@all << self 
-  end 
-  
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def self.new_by_filename(filename)
-    song_name=filename.split(" - ")[1]
-    artist=filename.split(" - ")[0]
-    song=self.new(song_name)
-    song.artist_name=artist
-    song 
-  end 
-  
+    @name = name
+  end
+
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
   def artist_name=(name)
-    self.artist=Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-    
-  end 
-    
-  
-  
-  
-  
-  
-  
-  
-end 
+  end
+
+end
